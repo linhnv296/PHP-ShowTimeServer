d682df58a7a9b4ab8ee8d0e39af653c33ab3c300
linhnv296 <linhnv296@gmail.com>
2019-06-03 15:16:17 +0700

    update m to i

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
     <title>Thời gian hiện tại</title>
 </head>
 <body>
-<h1>Bây giờ là: </h1><?php echo date('Y, M, d h:m:s');?>
+<h1>Bây giờ là: </h1><?php echo date('Y, M, d h:i:s');?>
 </body>
 </html>
